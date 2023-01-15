17ce4040b6ca64bbe53e957d019251ee55d99697
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2023-01-15 03:19:04 +0000

    test: 💍 get method
    added a test to validate if the get method retrieves mangas from the
    scrapped website

---
+++ src/test/routes/mangas.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../../app');
+
+test('Test #1 - Validates if the get method retrieves all mangas from the website', async () => {
+    const response = await request(app).get('/mangas');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+
+    console.log(response.body);
+});
