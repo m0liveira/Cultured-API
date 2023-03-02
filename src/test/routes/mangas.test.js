bcc9e184f1dd383fbf40ba88b613b8d81de1c0c6
Mateus Oliveira <mateusamaraloliveira160302@gmail.com>
2023-03-02 09:40:11 +0000

    test: 💍 trending mangas endpoint
    tested if the trending mangas endpoint is returning values from the call
    made

--- src/test/routes/mangas.test.js
+++ src/test/routes/mangas.test.js
@@ -3,8 +3,13 @@ const app = require('../../app');
 
 test('Test #1 - Validates if the get method retrieves mangas from the website', async () => {
     const response = await request(app).get('/mangas');
+    // #NOTE: It's a long time running test (it´s working)
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBeGreaterThan(0);
+});
 
-    console.log(response.body);
+test('Test #2 - Validates if the get method retrieves most popular mangas from the website', async () => {
+    const response = await request(app).get('/mangas/trending');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
 });
